6fba39d3e91539b9a8a26c964b13dcbaf78861e3
chcrain <chcrain94@gmail.com>
2025-01-29 19:47:40 -0500

    Fixed Cloudflare R2 Upload with AWS SDK

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const morgan = require('morgan');
 const app = express();
 
 // Define required environment variables and Google Script URL
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwDCXj7m6vTn_JeVKxHnqfas5q94U43K9C-ppJur14IUOtvypFrVirNFuxKkrbmRInV/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz573LmUq08zvQDY7Fmiruvv_8n3mDh-w6QAPLTsLNIXzlXABcNNagbdBPpRNKMAZQS/exec';
 const REQUIRED_ENV_VARS = [
   'CLOUDFLARE_ACCOUNT_ID',
   'CLOUDFLARE_BUCKET_NAME',
